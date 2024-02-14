8aa0efc6d2960ee85766e08369af411c1183cf4f
Changseop Yoon/윤창섭 <68686603+ryunchang@users.noreply.github.com>
2024-02-14 13:43:06 +0900

    Update k8s_example.py

--- dags/k8s_example.py
+++ dags/k8s_example.py
@@ -28,7 +28,7 @@ with DAG(
         in_cluster=False,  # false로 한다. 
         cluster_context='k3s-test', # context 를 설정한다. context 가 하나면 설정 안해도 됨
         config_file='/bitnami/airflow/kube-config/config-k3stest', # kube config 화일을 설정한다.
-        namespace="airflow", #  namespace를 설정한다.
+        namespace="default", #  namespace를 설정한다.
         image="python:3.10-slim",  # 이 이미지에 필요한 파이썬 스크립트와 의존성이 포함되어 있어야 합니다.
         is_delete_operator_pod=False,
         cmds=["python", "-c"],
